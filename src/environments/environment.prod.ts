1c45692282fd75b5857d71ed39ade9f447b7bc9e
OYEWOLE OLUWASEUN <oyewoleseun22@gmail.com>
2024-03-14 13:05:26 +0000

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDfMaW2TWjo********************",
+    authDomain: "netflixclone222.firebaseapp.com",
+    projectId: "netflixclone222",
+    storageBucket: "netflixclone222.appspot.com",
+    messagingSenderId: "2890***********",
+    appId: "1:289086747432:web:4fb23c463*******************",
+    measurementId: "G-WR64*************"
+  },
+  production: true
+};
